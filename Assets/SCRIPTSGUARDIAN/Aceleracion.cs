542e0aeeb8b327f1328d89b258c5063931733be7
Jesus Rodriguez Echenique <j.eh.u14@hotmail.com>
2024-10-19 09:00:32 -0500

    Agregando Aceleracion Progresiva,Jesus

--- Assets/SCRIPTSGUARDIAN/Aceleracion.cs
+++ Assets/SCRIPTSGUARDIAN/Aceleracion.cs
@@ -2,17 +2,100 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Aceleracion: GTemplo
 {
-    // Start is called before the first frame update
+    public float aceleracion;
+    private float velocidadMaxima;
+    private float aceleracionBase;
+    private bool persiguiendoJugador = false;
+
+    private Transform jugador;
+
+
     void Start()
     {
-        
+        base.Start();
+        velocidadMaxima = velocidad;
+        velocidad = 0;
+        aceleracionBase = aceleracion;
+        GameObject jugadorObj = GameObject.FindWithTag("JUGADOR");
+        if (jugadorObj != null)
+        {
+            jugador = jugadorObj.transform;
+        }
+        else
+        {
+            Debug.LogWarning("No se encontró al jugador en la escena.");
+        }
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+        base.Update();
+        if (jugador != null && JugadorEnRango())
+        {
+            if (!persiguiendoJugador)
+            {
+                persiguiendoJugador = true;
+                aceleracion += aceleracionBase;
+            }
+
+            velocidad += aceleracion * Time.deltaTime;
+            velocidad = Mathf.Clamp(velocidad, 0, velocidadMaxima);
+
+
+            transform.position = Vector3.MoveTowards(transform.position, jugador.position, velocidad * Time.deltaTime);
+
+
+            if (Time.time >= proximoAtaque && Vector3.Distance(transform.position, jugador.position) < rangoDeAtaque)
+            {
+                Atacar();
+                proximoAtaque = Time.time + tiempoEntreAtaques;
+            }
+        }
+        else
+        {
+            RegresarAPosicionOriginal();
+            velocidad = 0;
+            persiguiendoJugador = false;
+        }
+
     }
+
+    private bool JugadorEnRango()
+    {
+
+        return jugador != null && Vector3.Distance(transform.position, jugador.position) < rangoDeAtaque;
+    }
+
+    public override void Atacar()
+    {
+
+        if (jugadorScript != null)
+        {
+            jugadorScript.RecibirDanio(10);
+            Debug.Log("Atacando al jugador");
+        }
+        else
+        {
+            Debug.LogWarning("No se encontró el script del jugador.");
+        }
+    }
+
+    private void RegresarAPosicionOriginal()
+    {
+
+        if (puntosDePatrulla.Length > 0)
+        {
+            Patrullar();
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, posicionOriginal, velocidad * Time.deltaTime);
+        }
+    }
+
 }
+
+
