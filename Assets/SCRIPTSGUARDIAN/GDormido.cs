905f2b3dfea4b1cdd6f07a9d2d2f6902ebd0b5d7
Jesus Rodriguez Echenique <j.eh.u14@hotmail.com>
2024-10-19 08:29:03 -0500

    Agregando ESTADO DORMIDO,Jesus

--- Assets/SCRIPTSGUARDIAN/GDormido.cs
+++ Assets/SCRIPTSGUARDIAN/GDormido.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GDormido : MonoBehaviour
+public class GDormido : GTemplo
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
-        
+        base.Update();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
