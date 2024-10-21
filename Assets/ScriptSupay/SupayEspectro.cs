f583d9cc86dc6d99ed65e9510a23440f3371e49b
Jesus Rodriguez Echenique <j.eh.u14@hotmail.com>
2024-10-21 00:39:35 -0500

    Agregando mecanicnas finales al SUPAY ESPECTRO

--- Assets/ScriptSupay/SupayEspectro.cs
+++ Assets/ScriptSupay/SupayEspectro.cs
@@ -4,56 +4,48 @@ using UnityEngine;
 
 public class SupayEspectro : MonoBehaviour
 {
-
-    [SerializeField] private float rangoDeteccion = 10f;    
-    [SerializeField] private float tiempoPersecucion = 5f;  
+    [SerializeField] private float rangoDeteccion = 10f; 
     [SerializeField] private float velocidadPersecucion = 4f; 
-    [SerializeField] private float dañoPorGolpe = 10f;
-    [SerializeField] private float intervaloDeDaño = 2f;
+    [SerializeField] private float dañoPorGolpe = 10f; 
+    [SerializeField] private float intervaloDeDaño = 2f; 
     [SerializeField] private float porcentajeCordura = 0.3f;
 
-    private Transform jugador;
-    private Vector3 posicionInicial;
-    private bool persiguiendo = false;
-    private float tiempoRestantePersecucion;
+    private Transform jugador; 
+    private bool persiguiendo = false; 
     private float tiempoUltimoGolpe;
+
 
     private void Start()
     {
-        posicionInicial = transform.position; 
-        tiempoRestantePersecucion = tiempoPersecucion;
-        tiempoUltimoGolpe = 0f;
+        tiempoUltimoGolpe = 0f; 
     }
 
     private void Update()
     {
         if (jugador != null)
         {
+            
             if (Vector3.Distance(transform.position, jugador.position) <= rangoDeteccion)
             {
-                PerseguirJugador();
-
-
                 if (!persiguiendo)
                 {
-                    persiguiendo = true;
-                    tiempoRestantePersecucion = tiempoPersecucion;
+                    
+                    int corduraAFijar = (int)(jugador.GetComponent<Player>().GetCordura() * porcentajeCordura);
+                    jugador.GetComponent<Player>().ReducirCordura(corduraAFijar);
+                    persiguiendo = true; 
                 }
+                PerseguirJugador();
             }
-            else if (persiguiendo)
+            else
             {
-                RegresarAPosicionInicial();
+                persiguiendo = false; 
             }
-        }
-        else 
-        {
-            RegresarAPosicionInicial();
         }
     }
 
     private void PerseguirJugador()
     {
-
+        
         transform.position = Vector3.MoveTowards(transform.position, jugador.position, velocidadPersecucion * Time.deltaTime);
 
         
@@ -66,72 +58,15 @@ public class SupayEspectro : MonoBehaviour
             
             if (jugador.GetComponent<Player>().GetVida() <= 0)
             {
-                Debug.Log("ESTÁ MUERTO");
+                Debug.Log("ESTÁS MUERTO");
                 Destroy(jugador.gameObject); 
             }
-            TeletransportarYReducirCordura();
         }
-
-        tiempoRestantePersecucion -= Time.deltaTime;
-        if (tiempoRestantePersecucion <= 0) 
-        {
-            RegresarAPosicionInicial();
-        }
-    }
-
-    private void TeletransportarYReducirCordura()
-    {
-
-        StartCoroutine(TeletransportarCoroutine());
-    }
-
-    private IEnumerator TeletransportarCoroutine()
-    {
-       
-        gameObject.SetActive(false); 
-
-        
-        yield return new WaitForSeconds(0.1f);
-
-        
-        Vector3 direccionAlJugador = (jugador.position - transform.position).normalized;
-        Vector3 nuevaPosicion = jugador.position - direccionAlJugador * 1.5f; 
-        transform.position = nuevaPosicion;
-
-        
-        gameObject.SetActive(true); 
-
-        
-        int corduraAFijar = (int)(jugador.GetComponent<Player>().GetCordura() * porcentajeCordura);
-        jugador.GetComponent<Player>().ReducirCordura(corduraAFijar);
-
-        
-        if (jugador.GetComponent<Player>().GetCordura() <= 0)
-        {
-            Debug.Log("EL JUGADOR SE VOLVIO LOCO");
-            Destroy(jugador.gameObject); 
-        }
-    }
-
-    private void RegresarAPosicionInicial()
-    {
-        if (persiguiendo) 
-        {
-            persiguiendo = false;
-            transform.position = Vector3.MoveTowards(transform.position, posicionInicial, velocidadPersecucion * Time.deltaTime);
-        }
-    }
-
-    private void OnDrawGizmos()
-    {
-       
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, rangoDeteccion);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-       
+        
         if (other.CompareTag("PLAYER"))
         {
             jugador = other.transform;
@@ -144,7 +79,14 @@ public class SupayEspectro : MonoBehaviour
         if (other.CompareTag("PLAYER"))
         {
             jugador = null;
-            RegresarAPosicionInicial();
+            persiguiendo = false; 
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, rangoDeteccion);
     }
 }
